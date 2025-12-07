c38bf9fa3f86c3dc94e72eed707bce0e24ff3a59
Isaias-Alves <isaias.alves@sga.pucminas.br>
2025-12-07 20:13:40 -0300

    feat: entity criado

---
+++ src/main/java/com/openflightsapi/entity/Airport.java
@@ -0,0 +1,149 @@
+package com.openflightsapi.aeroports.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Airport {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(length = 500)
+    private String name;
+
+    private String city;
+    private String country;
+
+    @Column(length = 10)
+    private String iata;
+
+    @Column(length = 10)
+    private String icao;
+
+    private Double latitude;
+    private Double longitude;
+    private Integer altitude;
+    private String timezone;
+    private String dst;
+    private String tzDb;
+    private String type;
+    private String source;
+
+    public Airport() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getIata() {
+        return iata;
+    }
+
+    public void setIata(String iata) {
+        this.iata = iata;
+    }
+
+    public String getIcao() {
+        return icao;
+    }
+
+    public void setIcao(String icao) {
+        this.icao = icao;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Integer getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(Integer altitude) {
+        this.altitude = altitude;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public String getDst() {
+        return dst;
+    }
+
+    public void setDst(String dst) {
+        this.dst = dst;
+    }
+
+    public String getTzDb() {
+        return tzDb;
+    }
+
+    public void setTzDb(String tzDb) {
+        this.tzDb = tzDb;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+}
